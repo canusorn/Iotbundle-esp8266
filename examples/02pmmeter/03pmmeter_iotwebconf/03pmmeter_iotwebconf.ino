9571dbf9e166d1fdca1f8312a47caf84ed3737fc
canusorn <39732595+canusorn@users.noreply.github.com>
2022-03-01 17:36:32 +0700

    add pmmeter example

---
+++ examples/02pmmeter/03pmmeter_iotwebconf/03pmmeter_iotwebconf.ino
@@ -0,0 +1,284 @@
+/*
+   -PMS7003-
+   5V - VCC
+   GND - GND
+   D4  - TX
+   D3  - RX(not use in this code)
+
+*/
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <IotWebConf.h>
+#include <IotWebConfUsing.h>
+#include <ESP8266HTTPUpdateServer.h>
+#include <ESP8266mDNS.h>
+#include <Wire.h>
+#include <SFE_MicroOLED.h>
+#include <PMS.h>
+#include <SoftwareSerial.h>
+#include <iotbundle.h>
+
+// 1 สร้าง object ชื่อ iot และกำหนดค่า(project)
+#define PROJECT "PM_METER"
+Iotbundle iot(PROJECT);
+
+const char thingName[] = "pmmeter";
+const char wifiInitialApPassword[] = "iotbundle";
+
+#define STRING_LEN 128
+#define NUMBER_LEN 32
+
+#define CONFIG_VERSION "0.0.1"
+#define CONFIG_PIN D5
+
+// -- Method declarations.
+void handleRoot();
+// -- Callback methods.
+void wifiConnected();
+void configSaved();
+bool formValidator(iotwebconf::WebRequestWrapper *webRequestWrapper);
+
+SoftwareSerial pmsSerial(D4, D3); // RX,TX
+PMS pms(pmsSerial);
+PMS::DATA data;
+
+#define PIN_RESET -1
+#define DC_JUMPER 0
+
+MicroOLED oled(PIN_RESET, DC_JUMPER);
+
+unsigned long previousMillis = 0, currentMillis = 0;
+
+DNSServer dnsServer;
+WebServer server(80);
+ESP8266HTTPUpdateServer httpUpdater;
+
+char emailParamValue[STRING_LEN];
+char passParamValue[STRING_LEN];
+char serverParamValue[STRING_LEN];
+
+IotWebConf iotWebConf(thingName, &dnsServer, &server, wifiInitialApPassword, CONFIG_VERSION);
+// -- You can also use namespace formats e.g.: iotwebconf::TextParameter
+IotWebConfParameterGroup login = IotWebConfParameterGroup("login", "ล็อกอิน(สมัครที่เว็บก่อนนะครับ)");
+
+IotWebConfTextParameter emailParam = IotWebConfTextParameter("อีเมลล์", "emailParam", emailParamValue, STRING_LEN);
+IotWebConfPasswordParameter passParam = IotWebConfPasswordParameter("รหัสผ่าน", "passParam", passParamValue, STRING_LEN);
+IotWebConfTextParameter serverParam = IotWebConfTextParameter("เซิฟเวอร์", "serverParam", serverParamValue, STRING_LEN, "https://iotkiddie.com");
+
+uint8_t logo_bmp[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0xC0, 0xE0, 0xC0, 0xF0, 0xE0, 0x78, 0x38, 0x78, 0x3C, 0x1C, 0x3C, 0x1C, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1C, 0x3C, 0x1C, 0x3C, 0x78, 0x38, 0xF0, 0xE0, 0xF0, 0xC0, 0xC0, 0xC0, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x03, 0x03, 0x01, 0x00, 0x00, 0xF0, 0xF8, 0x70, 0x3C, 0x3C, 0x1C, 0x1E, 0x1E, 0x0E, 0x0E, 0x0E, 0x0F, 0x0F, 0x0E, 0x0E, 0x1E, 0x1E, 0x1E, 0x3C, 0x1C, 0x7C, 0x70, 0xF0, 0x70, 0x20, 0x01, 0x01, 0x03, 0x03, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x1C, 0x3E, 0x1E, 0x0F, 0x0F, 0x07, 0x87, 0x87, 0x07, 0x0F, 0x0F, 0x1E, 0x3E, 0x1C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x06, 0x1F, 0x1F, 0x3F, 0x3F, 0x1F, 0x1F, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t wifi_on[] = {0x08, 0x04, 0x12, 0xCA, 0xCA, 0x12, 0x04, 0x08};
+uint8_t wifi_off[] = {0x88, 0x44, 0x32, 0xDA, 0xCA, 0x16, 0x06, 0x09};
+uint8_t wifi_ap[] = {0x3E, 0x41, 0x1C, 0x00, 0xF8, 0x00, 0x1C, 0x41, 0x3E};
+uint8_t wifi_nointernet[] = {0x04, 0x12, 0xCA, 0xCA, 0x12, 0x04, 0x5F, 0xDF};
+uint8_t t_connecting;
+
+void setup()
+{
+  Serial.begin(115200);
+  pmsSerial.begin(9600);
+
+  //------Display LOGO at start------
+  oled.begin();
+  oled.clear(PAGE);
+  oled.clear(ALL);
+  oled.drawBitmap(logo_bmp); // call the drawBitmap function and pass it the array from above
+  oled.setFontType(0);
+  oled.setCursor(0, 36);
+  oled.print(" IoTbundle");
+  oled.display();
+
+  login.addItem(&emailParam);
+  login.addItem(&passParam);
+  login.addItem(&serverParam);
+
+  //  iotWebConf.setStatusPin(STATUS_PIN);
+  iotWebConf.setConfigPin(CONFIG_PIN);
+  //  iotWebConf.addSystemParameter(&stringParam);
+  iotWebConf.addParameterGroup(&login);
+  iotWebConf.setConfigSavedCallback(&configSaved);
+  iotWebConf.setFormValidator(&formValidator);
+  iotWebConf.getApTimeoutParameter()->visible = false;
+  iotWebConf.setWifiConnectionCallback(&wifiConnected);
+
+  // -- Define how to handle updateServer calls.
+  iotWebConf.setupUpdateServer(
+      [](const char *updatePath)
+      {
+        httpUpdater.setup(&server, updatePath);
+      },
+      [](const char *userName, char *password)
+      {
+        httpUpdater.updateCredentials(userName, password);
+      });
+
+  // -- Initializing the configuration.
+  iotWebConf.init();
+
+  // -- Set up required URL handlers on the web server.
+  server.on("/", handleRoot);
+  server.on("/config", []
+            { iotWebConf.handleConfig(); });
+  server.onNotFound([]()
+                    { iotWebConf.handleNotFound(); });
+
+  Serial.println("Ready.");
+}
+
+void loop()
+{
+  iotWebConf.doLoop();
+  server.handleClient();
+  MDNS.update();
+
+  // 3 คอยจัดการ และส่งค่าให้เอง
+  iot.handle();
+
+  //------get data from PMS7003------
+  if (pms.read(data))
+  {
+    display_update();
+
+    /*  4 เมื่อได้ค่าใหม่ ให้อัพเดทตามลำดับตามตัวอย่าง
+        ตัวไลบรารี่รวบรวมและหาค่าเฉลี่ยส่งขึ้นเว็บให้เอง
+        ถ้าค่าไหนไม่ต้องการส่งค่า ให้กำหนดค่าเป็น NAN   */
+    iot.update(data.PM_AE_UG_1_0, data.PM_AE_UG_2_5, data.PM_AE_UG_10_0);
+    previousMillis = currentMillis;
+  }
+
+  //------No response from PMS7003 in 2 second------
+  currentMillis = millis();
+  if (currentMillis - previousMillis >= 2000)
+  {
+    previousMillis = currentMillis;
+    oled.clear(PAGE);
+    oled.setFontType(0);
+    oled.setCursor(0, 0);
+    oled.println("No Sensor");
+    oled.print("detect!");
+    oled.display();
+  }
+}
+
+void display_update()
+{
+  //------Update OLED------
+  oled.clear(PAGE);
+  oled.setFontType(0);
+  oled.setCursor(0, 0);
+  oled.println("PM [ug/m3]");
+  oled.setCursor(0, 15);
+  oled.print(" 1.0 : ");
+  oled.print(data.PM_AE_UG_1_0);
+  oled.setCursor(0, 26);
+  oled.print(" 2.5 : ");
+  oled.print(data.PM_AE_UG_2_5);
+  oled.setCursor(0, 37);
+  oled.print("10.0 : ");
+  oled.print(data.PM_AE_UG_10_0);
+
+  // display state
+  if (iotWebConf.getState() == 1 || iotWebConf.getState() == 2)
+    oled.drawIcon(55, 0, 9, 8, wifi_ap, sizeof(wifi_ap), true);
+  else if (iotWebConf.getState() == 3)
+  {
+    if (t_connecting == 1)
+    {
+      oled.drawIcon(56, 0, 8, 8, wifi_on, sizeof(wifi_on), true);
+      t_connecting = 0;
+    }
+    else
+    {
+      t_connecting = 1;
+    }
+  }
+  else if (iotWebConf.getState() == 4)
+  {
+    if (iot.serverConnected)
+      oled.drawIcon(56, 0, 8, 8, wifi_on, sizeof(wifi_on), true);
+    else
+      oled.drawIcon(56, 0, 8, 8, wifi_nointernet, sizeof(wifi_nointernet), true);
+  }
+  else if (iotWebConf.getState() == 5)
+    oled.drawIcon(56, 0, 8, 8, wifi_off, sizeof(wifi_off), true);
+
+  oled.display();
+
+  //------print on serial moniter------
+  Serial.print("PM 1.0 (ug/m3): ");
+  Serial.println(data.PM_AE_UG_1_0);
+  Serial.print("PM 2.5 (ug/m3): ");
+  Serial.println(data.PM_AE_UG_2_5);
+  Serial.print("PM 10.0 (ug/m3): ");
+  Serial.println(data.PM_AE_UG_10_0);
+}
+
+void handleRoot()
+{
+  // -- Let IotWebConf test and handle captive portal requests.
+  if (iotWebConf.handleCaptivePortal())
+  {
+    // -- Captive portal request were already served.
+    return;
+  }
+  String s = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>";
+  s += "<title>Iotkiddie AC Powermeter config</title></head><body>IoTkiddie config data";
+  s += "<ul>";
+  s += "<li>Device name : ";
+  s += String(iotWebConf.getThingName());
+  s += "<li>อีเมลล์ : ";
+  s += emailParamValue;
+  s += "<li>ESP ID : ";
+  s += ESP.getChipId();
+  s += "<li>Server : ";
+  s += serverParamValue;
+  s += "</ul>";
+  s += "Go to <a href='config'>configure page</a> to change values.";
+  s += "</body></html>\n";
+
+  server.send(200, "text/html", s);
+}
+
+void configSaved()
+{
+  Serial.println("Configuration was updated.");
+}
+
+void wifiConnected()
+{
+
+  Serial.println("WiFi was connected.");
+  MDNS.begin(iotWebConf.getThingName());
+  MDNS.addService("http", "tcp", 80);
+
+  Serial.printf("Ready! Open http://%s.local in your browser\n", String(iotWebConf.getThingName()));
+  if ((String)emailParamValue != "" && (String)passParamValue != "")
+  {
+    Serial.println("login");
+
+    // 2 เริ่มเชื่อมต่อ หลังจากต่อไวไฟได้
+    if ((String)passParamValue != "")
+      iot.begin((String)emailParamValue, (String)passParamValue, (String)serverParamValue);
+    else // ถ้าไม่ได้ตั้งค่า server ให้ใช้ค่า default
+      iot.begin((String)emailParamValue, (String)passParamValue);
+  }
+}
+
+bool formValidator(iotwebconf::WebRequestWrapper *webRequestWrapper)
+{
+  Serial.println("Validating form.");
+  bool valid = true;
+
+  /*
+    int l = webRequestWrapper->arg(stringParam.getId()).length();
+    if (l < 3)
+    {
+      stringParam.errorMessage = "Please provide at least 3 characters for this test!";
+      valid = false;
+    }
+  */
+  return valid;
+}
